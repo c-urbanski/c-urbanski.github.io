f73efa6f77494fd39e5b4f23001dbf1ea11d028d
c-urbanski <chris.urbanski0@gmail.com>
2023-06-08 20:44:11 -0400

    added margin when zooming images

--- assets/js/zoom.js
+++ assets/js/zoom.js
@@ -1,6 +1,7 @@
 // Initialize medium zoom.
 $(document).ready(function() {
   medium_zoom = mediumZoom('[data-zoomable]', {
+    margin: 60,
     background: getComputedStyle(document.documentElement)
         .getPropertyValue('--global-bg-color') + 'ee',  // + 'ee' for trasparency.
   })
